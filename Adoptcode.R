38c1834eb8ece585a79ec7f6f97bb995278d3d2a
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2020-03-17 13:01:28 -0700

    fixed merge issue

--- Adoptcode.R
+++ Adoptcode.R
@@ -8,7 +8,7 @@ library(patchwork)
 library(cowplot)
 library(ggsci)
 
-### Use the adopt a pet website
+### Use the adopt a pet website for los angeles
 startUrl<-"https://www.adoptapet.com/adoption_rescue/4223-kitten-rescue-los-angeles-california"
 
 # make a function to pull out all the data from the website and make a datafram
@@ -16,6 +16,7 @@ getCats<-function(i) {
   cat(i, "\n")
   url <- str_c(startUrl, "?page=", i)
   
+  # pull in the URL
 webpage <- read_html(url)
 
 b<-webpage %>%
@@ -45,20 +46,23 @@ pics<-webpage %>%
  html_nodes("img") %>% 
   map(xml_attrs) %>% 
   map_df(~as.list(.))%>%
-  slice(-c(nrow(.)-20:nrow(.))) %>%
+  filter(is.na(class))%>%
+  #slice(-c(nrow(.)-20:nrow(.))) %>%
   select(src)
 
-b<-bind_cols(b,pics)
+b<-merge(b,pics)
+
+#b<-bind_cols(b,pics)
 return(b)
 }
 
 # extract the cat data
 catdata <- c(1:11) %>% # how many pages of data to look through
-  map_dfr(getCats)
-# something is wrong on page 9
+  map_dfr(getCats) # stack the data on top of each other
 
 ## Make some plots
 # Cats by breed
+
 p1<-catdata %>%
   filter(Breed !="")%>%
   count(Breed, Age, Sex)%>%
@@ -70,18 +74,19 @@ p1<-catdata %>%
   ggtitle("Cats up for adoption")+
   scale_fill_uchicago()+
   theme_classic()+
-  theme(axis.text.x = element_text(angle = 90, size = 10),
+  theme(axis.text.x = element_text(angle = 90, size = 10, vjust = 0.5, hjust = 1),
         axis.title = element_text(size = 14),
         plot.title = element_text(size = 16, hjust = 0.5),
-        #legend.position = "none",
         panel.grid = element_blank(),
-        #plot.caption = element_text(hjust = 0.5, size = 16, vjust = 10),
-        legend.title = element_blank()
+        legend.title = element_blank(),
+        strip.background = element_blank()
+        
+       # plot.background = element_rect(fill = "cornsilk", color = NA)
         ) +
   facet_wrap(~Sex, nrow = 2)
 
 
-## Pick a random cat
+## Pick a random cat to view
 catnum<-sample(nrow(catdata),1,replace=T)
 
 # give its info
@@ -90,6 +95,7 @@ Breed<-catdata$Breed[catnum]
 Age<-catdata$Age[catnum]
 Sex<-catdata$Sex[catnum]
 
+# write a statement
 cat.info<-glue('My name is {Name}.\n I am a {Sex} {Breed} ({Age}).\n You can adopt me at adoptapet.com')
 
 # put a cat on it
@@ -105,6 +111,7 @@ p4<-ggdraw() +
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
 
+# pull the plot together
   p1 +p4+
   plot_annotation(title = paste("There are",nrow(catdata), "adoptable cats in Los Angeles."))+ 
   theme(plot.title = element_text(size = 20))+

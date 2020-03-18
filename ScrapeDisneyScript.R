f9495d752d56f94756170a877c5e2e3f52c5b652
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2020-03-18 11:39:04 -0700

    scraped harry potter names

--- ScrapeDisneyScript.R
+++ ScrapeDisneyScript.R
@@ -13,4 +13,23 @@ disney<-webpage %>%
   slice(-c(1008:1022))%>%
   rename("Character" = ".")
   
+## scrape Harry Potter
 
+
+ url<-"https://en.wikipedia.org/wiki/List_of_Harry_Potter_characters"
+ # pull in the URL
+ webpage <- read_html(url)
+ 
+ 
+ HP<-webpage %>%
+   html_nodes("a") %>% # extract the data
+   html_text(trim = TRUE)%>%
+   data.frame() %>%
+   slice(-c(1:77)) %>%
+   slice(-c(352:594))%>%
+   rename("Character" = ".")%>%
+   mutate(num.char = nchar(as.character(Character)))%>%
+   filter(num.char>1) %>% # remove the letter of the alphabet
+   select(Character)
+
+  
